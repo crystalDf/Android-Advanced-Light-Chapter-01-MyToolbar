62029c3abae6971a797089bfcee8b37116ec3856
crystalDf <chendong333@gmail.com>
2018-07-03 23:30:40 +0800

    MyToolbar

--- app/src/main/java/com/star/mytoolbar/ToolbarActivity.java
+++ app/src/main/java/com/star/mytoolbar/ToolbarActivity.java
@@ -1,8 +1,9 @@
 package com.star.mytoolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class ToolbarActivity extends AppCompatActivity {
 
@@ -19,7 +20,36 @@ public class ToolbarActivity extends AppCompatActivity {
     private void initViews() {
 
         mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle("Toolbar");
 
         setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_launcher_foreground);
+
+        mToolbar.setOnMenuItemClickListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case R.id.settings:
+                    break;
+                case R.id.share:
+                    break;
+                case R.id.search:
+                    break;
+                default:
+
+            }
+
+            return true;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
     }
 }
