a2a328498d348a7f370c7c390b767afbce3320ce
crystalDf <chendong333@gmail.com>
2018-07-07 19:38:31 +0800

    MyToolbar

--- app/src/main/java/com/star/mytoolbar/ToolbarActivity.java
+++ app/src/main/java/com/star/mytoolbar/ToolbarActivity.java
@@ -4,14 +4,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class ToolbarActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +29,19 @@ public class ToolbarActivity extends AppCompatActivity {
 
     private void initViews() {
 
+        setToolbar();
+        setDrawerLayout();
+        setPalette();
+    }
+
+    private void setToolbar() {
         mToolbar = findViewById(R.id.toolbar);
         mToolbar.setTitle("Toolbar");
 
         setSupportActionBar(mToolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.bitmap);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
 
         mToolbar.setOnMenuItemClickListener(item -> {
 
@@ -47,7 +59,24 @@ public class ToolbarActivity extends AppCompatActivity {
 
             return true;
         });
+    }
 
+    private void setDrawerLayout() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mTextView = findViewById(R.id.close);
+
+        ActionBarDrawerToggle actionBarDrawerToggle =
+                new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                        R.string.drawer_open, R.string.drawer_close);
+        actionBarDrawerToggle.syncState();
+
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        mTextView.setOnClickListener(v -> mDrawerLayout.closeDrawer(Gravity.START));
+
+    }
+
+    private void setPalette() {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bitmap);
 
         Palette.from(bitmap).generate(palette -> {
